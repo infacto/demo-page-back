1e45b6f5652c8e642a933246b620edb56e2be123
infacto <60390085+infacto@users.noreply.github.com>
2020-05-12 07:40:30 +0200

    initial commit

---
+++ src/app/pages/overview/overview.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.page.html',
+})
+export class OverviewPage {
+  public title = 'Overview';
+}
