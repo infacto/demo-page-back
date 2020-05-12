2d6cb850e97f12ce599dec99d3e945aa10843bfa
infacto <60390085+infacto@users.noreply.github.com>
2020-05-12 07:33:59 +0200

    initial commit

---
+++ src/data/items.ts
@@ -0,0 +1,19 @@
+export interface Item {
+  id: string;
+  name: string;
+}
+
+export const items: Item[] = [
+  {
+    id: 'baaaa858-9adf-42f9-ab5a-34cbe1ba5dd2',
+    name: 'One',
+  },
+  {
+    id: '25d13fe2-b964-4d4f-b010-5da42aa20b56',
+    name: 'Two',
+  },
+  {
+    id: '17c89451-19d4-48d8-95ee-54be71953d37',
+    name: 'Three',
+  },
+];
